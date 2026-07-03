94ab1ba7e65122f6b822bf8be382dcc637544894
DLIF\ice <ice@signumlifescience.com>
2026-07-03 15:46:16 +0200

    chore: Remove unused files

--- .github/scripts/repair_workspace_permissions.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-sudo chown -R runner:guest "$GITHUB_WORKSPACE"
-sudo chmod -R g+rwX "$GITHUB_WORKSPACE"
-sudo find "$GITHUB_WORKSPACE" -type d -exec chmod g+s {} +
